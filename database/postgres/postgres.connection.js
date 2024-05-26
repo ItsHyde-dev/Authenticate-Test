c264823ff05a1dfc9cad7ce66bc06395125d1c9b
Himanshu Joshi <heyitshyde@gmail.com>
2024-05-26 20:25:50 +0530

    updated postman collection

--- database/postgres/postgres.connection.js
+++ database/postgres/postgres.connection.js
@@ -1,2 +1,12 @@
-import { Sequelize } from 'sequelize'
-export const sequelize = new Sequelize(process.env.DB_CONN_URI)
+import { Sequelize } from "sequelize";
+export const sequelize = new Sequelize(process.env.DB_CONN_URI, {
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+      ca: process.env.DB_CONN_CERT,
+      cert: process.env.DB_CONN_CERT,
+    },
+  },
+});
